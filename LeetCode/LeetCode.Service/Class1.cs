a5fd3bdd8df1f454a8971797cb3a002388636d18
zhaoweijie0817 <zhaoweijie0817>
2020-09-28 11:00:05 +0800

    准备

--- LeetCode/LeetCode.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LeetCode.Service
-{
-    public class Class1
-    {
-    }
-}
---
+++ LeetCode/LeetCode.Service/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Service
+{
+    public class Service
+    {
+        #region 
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var s=l1.next;
+        }
+        #endregion
+    }
+    
+
+  public class ListNode
+    {
+      public int val;
+      public ListNode next;
+     public ListNode(int x) { val = x; }
+  }
+
+}
