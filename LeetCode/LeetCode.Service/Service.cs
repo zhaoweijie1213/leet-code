7c36b98fd7d8538a8cd363dafd7c2737f4bdbe8e
zhaoweijie <949210784@qq.com>
2020-12-15 16:58:13 +0800

    26. 删除排序数组中的重复项

--- LeetCode/LeetCode.Service/Service.cs
+++ LeetCode/LeetCode.Service/Service.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Service
 {
-    public class Service
+    public partial class Service
     {
         #region 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
@@ -12,6 +13,29 @@ namespace LeetCode.Service
             return l1;
         }
         #endregion
+
+        /// <summary>
+        /// 删除排序数组中的重复项
+        /// 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+        /// 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+        /// </summary>
+        /// https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    i++;
+                    nums[i] = nums[j];
+                }
+            }
+            return i + 1;
+        }
     }
 
 
@@ -21,5 +45,4 @@ namespace LeetCode.Service
         public ListNode next;
         public ListNode(int x) { val = x; }
     }
-
 }
